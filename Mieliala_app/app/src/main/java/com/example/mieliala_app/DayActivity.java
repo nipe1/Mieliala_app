f8a8a36df76f8ad8015412476b4bd696e04e1e96
t7kear00 <t7kear00@students.oamk.fi>
2020-03-23 16:46:19 +0200

    nakyman toteutus

---
+++ Mieliala_app/app/src/main/java/com/example/mieliala_app/DayActivity.java
@@ -0,0 +1,15 @@
+package com.example.mieliala_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+
+    }
+}
--- Mieliala_app/app/src/main/java/com/example/mieliala_app/MainActivity.java
+++ Mieliala_app/app/src/main/java/com/example/mieliala_app/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.mieliala_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    //väliaikainen intent id, jolla hypätään suoraan DayActivity -näkymään
+    int ADD_NEW_PART_INTENT_ID = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //siirrytään suoraan DayActivity -näkymään
+        Intent dayActivityIntent = new Intent(this, DayActivity.class);
+        startActivityForResult(dayActivityIntent, ADD_NEW_PART_INTENT_ID);
+
     }
 }
