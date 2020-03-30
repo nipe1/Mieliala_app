42850521b2253c1fb5547a0453040ee7bc1ba868
nipe1 <pelkonenniilo@gmail.com>
2020-03-30 16:30:25 +0300

    added functionality for main activity

--- Mieliala_app/app/src/main/java/com/example/mieliala_app/CalendarActivity.java
+++ Mieliala_app/app/src/main/java/com/example/mieliala_app/CalendarActivity.java
@@ -2,7 +2,10 @@ package com.example.mieliala_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String editFeel = sharedPreferences.getString("editKey", "");
+        int seekFeel = sharedPreferences.getInt("seekKey", 0);
+        String testval = editFeel + " " + String.valueOf(seekFeel);
+        TextView test = findViewById(R.id.textTest);
+        test.setText(testval);
     }
 }
--- Mieliala_app/app/src/main/java/com/example/mieliala_app/MainActivity.java
+++ Mieliala_app/app/src/main/java/com/example/mieliala_app/MainActivity.java
@@ -2,22 +2,52 @@ package com.example.mieliala_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    //väliaikainen intent id, jolla hypätään suoraan DayActivity -näkymään
+    EditText editFeel;
+    SeekBar seekFeel;
+    public static final String editKey = "editKey";
+    public static final String seekKey = "seekKey";
+    //väliaikainen intent id
     int ADD_NEW_PART_INTENT_ID = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekFeel = findViewById(R.id.seekFeeling);
+        editFeel = findViewById(R.id.editFeel);
+    }
 
-        //siirrytään suoraan DayActivity -näkymään
-        Intent dayActivityIntent = new Intent(this, DayActivity.class);
-        startActivityForResult(dayActivityIntent, ADD_NEW_PART_INTENT_ID);
+    private void goToIntent()
+    {
+        //siirrytään CalendarActivity -näkymään
+        Intent calendarActivityIntent = new Intent(this, CalendarActivity.class);
+        startActivityForResult(calendarActivityIntent, ADD_NEW_PART_INTENT_ID);
+    }
 
+    public void buttonNext(View v)
+    {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String c = editFeel.getText().toString();
+        int n = seekFeel.getProgress();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(editKey, c);
+        editor.putInt(seekKey, n);
+        editor.apply();
+        goToIntent();
+    }
+
+    public void buttonSkip(View v)
+    {
+        goToIntent();
     }
 }
