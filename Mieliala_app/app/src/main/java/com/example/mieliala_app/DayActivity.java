eaaf48744b065b73c1e601b2a94de89237fb3dae
nipe1 <pelkonenniilo@gmail.com>
2020-04-22 14:32:01 +0300

    small changes

--- Mieliala_app/app/src/main/java/com/example/mieliala_app/DayActivity.java
+++ Mieliala_app/app/src/main/java/com/example/mieliala_app/DayActivity.java
@@ -2,7 +2,6 @@ package com.example.mieliala_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
